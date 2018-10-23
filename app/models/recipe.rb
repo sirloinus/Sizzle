6bfa82e89b4ae0417ad1ee512fc12668102ff2f8
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-23 20:32:58 +0100

    creating ingredients_string method - still to complete

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,14 @@ class Recipe < ApplicationRecord
   has_many :user_recipes
   has_many :users, through: :user_recipes
 
+  def ingredients_string(string)
+    with_empty_strings = string.split(/[\p{punct}]+/)
+    # no_empty_strings = nil
+    # with_empty_strings.each do |s|
+    #   if !s.empty?
+    no_empty_strings = with_empty_strings.split(/\A[[:space:]]*\z/).flatten
+    no_empty_strings
+  end
+
+
 end
