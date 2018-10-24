4149a2fcf1f5fc5363f5d3c0769274eb11cc0a03
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-24 15:02:52 +0100

    added bulma framework and created navbar and basic user/show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
   helper_method :current_user, :logged_in?
 
-    # def current_user
-    #   @user = (User.find_by(id: session[:user_id]) || User.new)
-    # end
-
     def current_user
       if session[:user_id]
         User.find(session[:user_id])
@@ -15,33 +11,39 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    # def logged_in?
-    #   current_user.id != nil
-    # end
-
     def logged_in?
       !!current_user.id
     end
-    #
-    # def require_logged_in
-    #   return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
-    # end
-
+    
     def authorized?
       if !logged_in?
         flash[:authorized] = "You are not logged in"
         redirect_to login_path and return
       end
     end
+    #
+    # def authorized_for(user_id)
+    #   if current_user.admin == true
+    #     true
+    #   elsif current_user.id != user_id.to_i
+    #     flash[:authorized] = "You cannot view a page that's not authorized"
+    #     redirect_to current_user
+    #   end
+    # end
+    # #change it depends on our app. who is authorized?
 
-    def authorized_for(user_id)
-      if current_user.admin == true
-        true
-      elsif current_user.id != user_id.to_i
-        flash[:authorized] = "You cannot view a page that's not authorized"
-        redirect_to current_user
-      end
-    end
-    #change it depends on our app. who is authorized? 
+
+
+    # def current_user
+    #   @user = (User.find_by(id: session[:user_id]) || User.new)
+    # end
+    #
+    # def logged_in?
+    #   current_user.id != nil
+    # end
+    #
+    # def require_logged_in
+    #   return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+    # end
 
 end
