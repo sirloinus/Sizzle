8cd286f3463e7e6a21ca39ec9095ffdad00e1327
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-25 20:46:59 +0100

    created methods and flash errors for recipe searches that do not meet api conditions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  #   @user = User.create(user_params)
-  #   return redirect_to new_user_path unless @user.save
-  #   session[:user_id] = @user.id
-  #   redirect_to controller: 'users', action: 'show'
-  # end
-
   def create
     @user = User.new(user_params)
     if @user.valid?
@@ -28,6 +21,15 @@ class UsersController < ApplicationController
   def show
     # authorized_for(params[:id])
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[current_user.id])
+  end
+
+  def update
+    @dog.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
